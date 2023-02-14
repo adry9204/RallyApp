dd212bb8734b3f830d69a454d51ad102b3cb0bc1
adry9204 <adriana920420@gmail.com>
2023-02-14 03:23:23 -0500

    Sign up layout and navigation completed

--- app/src/main/java/com/example/rallyapp/SingUpActivity.kt
+++ app/src/main/java/com/example/rallyapp/SingUpActivity.kt
@@ -1,11 +1,30 @@
 package com.example.rallyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rallyapp.databinding.ActivityMainBinding
+import com.example.rallyapp.databinding.ActivitySingUpBinding
 
 class SingUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySingUpBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
+        binding = ActivitySingUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createUserButton.setOnClickListener(){
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.backToLoginButton.setOnClickListener(){
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
