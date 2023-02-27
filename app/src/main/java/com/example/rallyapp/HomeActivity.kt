c29c95a33c6a3b6510fa284d6521a1fd30dca155
adry9204 <adriana920420@gmail.com>
2023-02-27 18:21:46 -0500

    Home Page fully implemented with local products repo. Ready for integration

--- app/src/main/java/com/example/rallyapp/HomeActivity.kt
+++ app/src/main/java/com/example/rallyapp/HomeActivity.kt
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rallyapp.databinding.ActivityHomeBinding
 import com.example.rallyapp.databinding.FragmentHeaderBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-private lateinit var fragmentBinding: FragmentHeaderBinding
+    private lateinit var fragmentBinding: FragmentHeaderBinding
+    private var adapter: GridViewItemAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +29,17 @@ private lateinit var fragmentBinding: FragmentHeaderBinding
         transaction.commit()
 
         binding.pizza.bringToFront()
+
+        //creating and filling the Recycler View
+        val cardImages: Array<String> = resources.getStringArray(R.array.cardImages)
+        val cardTitles: Array<String> = resources.getStringArray(R.array.cardTitles)
+        val cardPrices: Array<String> = resources.getStringArray(R.array.cardPrices)
+
+        val gridViewItem = findViewById<RecyclerView>(R.id.home_menu_recyclerview)
+        gridViewItem.layoutManager = GridLayoutManager(this, 2)
+        adapter = GridViewItemAdapter(cardImages, cardTitles, cardPrices)
+
+        gridViewItem.adapter = adapter
     }
 
     fun goToUserActivity(v: View) {
