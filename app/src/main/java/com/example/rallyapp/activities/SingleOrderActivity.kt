8fc3fccae1b185553d34fc876ad62a32eb04742a
adry9204 <adriana920420@gmail.com>
2023-04-05 18:00:45 -0400

    Add to cart button glitch fixed

--- app/src/main/java/com/example/rallyapp/activities/SingleOrderActivity.kt
+++ app/src/main/java/com/example/rallyapp/activities/SingleOrderActivity.kt
@@ -1,6 +1,8 @@
 package com.example.rallyapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
